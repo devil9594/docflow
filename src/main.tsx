d956002ee76744d74afd079e14aadc0280a1c6e8
developer <vkumkar61@gmail.com>
2026-01-07 16:13:33 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-<BrowserRouter basename="/docflow">
-  <App />
-</BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/docflow">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
